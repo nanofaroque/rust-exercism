633020d1e92995cf0ce310a091d22eacb0723630
nanofaroque <faroque.ae@gmail.com>
2021-05-29 20:19:36 -0500

    armstrong number

--- prime-factors/src/lib.rs
+++ prime-factors/src/lib.rs
@@ -1,20 +1,20 @@
-use std::collections::HashSet;
 
-pub fn factors(n: u64) -> Vec<u64> {
-    let factor:u64=2;
-    let mut not_prime:HashSet<u64>=HashSet::new();
+pub fn factors(mut n: u64) -> Vec<u64> {
+
+    let mut factor:u64=2;
     let mut result:Vec<u64>=Vec::new();
-    while factor<n{
-      if !not_prime.contains(&factor){
-          if n%factor==0 {
-              result.push(factor);
-              let mut itr:u64=2;
-              while itr*factor<n {
-                  not_prime.insert(itr*factor);
-                  itr+=1;
-              }
-          }
-      }
+    if n==1 {
+        return result
     }
+    while factor<=n{
+      if n%factor==0{
+          result.push(factor);
+          n=n/factor;
+      }else {
+          factor+=1;
+      }
+
+    }
+    if n>1 {result.push(n);}
     return result;
 }
