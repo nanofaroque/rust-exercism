f639da77137688c68b96f8a131cbe4be27a9a203
nanofaroque <faroque.ae@gmail.com>
2021-05-31 23:17:38 -0500

    armstrong number

--- armstrong-numbers/src/lib.rs
+++ armstrong-numbers/src/lib.rs
@@ -1,3 +1,11 @@
 pub fn is_armstrong_number(num: u32) -> bool {
-    unimplemented!("true if {} is an armstrong number", num)
+    //unimplemented!("true if {} is an armstrong number", num)
+    let digits: Vec<_> = num.to_string().chars().map(|d| d.to_digit(10).unwrap()).collect();
+    print!("{}",digits.len());
+    let len: usize =digits.len();
+    let mut squared_sum:u32=0;
+    for n in digits{
+        squared_sum+=n.pow(len as u32)
+    }
+    return num==squared_sum;
 }
